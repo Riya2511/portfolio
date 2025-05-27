7a01a11a777d86bafcb4576e11132d943bfade77
Riya2511 <riyaj2709@gmail.com>
2025-05-28 02:23:38 +0530

    minor bug fix

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -1,5 +1,16 @@
+import Image from 'next/image';
+
 const TopLeftImg = () => {
-  return <div>Top Left Image</div>;
+  return <div className='absolute left-0 top-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[400px] opcaity-50'>
+
+    <Image
+      src="/top-left-img.png"
+      alt="Top Left Decorative Image"
+      width={400}
+      height={400}
+      className="absolute top-0 left-0 z-10"
+    />
+  </div>;
 };
 
 export default TopLeftImg;
