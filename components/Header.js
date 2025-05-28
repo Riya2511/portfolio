2ec58991f817c4ced6d602a2e1224e195e8e261c
Riya2511 <riyaj2709@gmail.com>
2025-05-29 01:04:19 +0530

    minor bug fix

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
         <Link href={'/'}>
           <Image 
-            src="/logo2.png"
+            src="/./logo2.png"
             width={170}
             height={40}
             alt='Logo'
