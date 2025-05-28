bc5bfcc850e76ca4685267888b0f83ae5a861a12
Riya2511 <riyaj2709@gmail.com>
2025-05-29 00:49:43 +0530

    home page completed

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,23 @@
+import Link from 'next/link';
+
+import {
+  RiInstagramLine,
+  RiGithubLine,
+  RiLinkedinLine,
+} from 'react-icons/ri';
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return <div className='flex items-center gap-x-5 text-2xl right-8'>
+    <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <RiInstagramLine />
+    </Link>
+    <Link href={'https://www.github.com/Riya2511'} className='hover:text-accent transition-all duration-300'>
+      <RiGithubLine />
+    </Link>
+    <Link href={'https://www.linkedin.com/in/riya-jain-226476205/'} className='hover:text-accent transition-all duration-300'>
+      <RiLinkedinLine />
+    </Link>
+  </div>;
 };
 
 export default Socials;
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,33 @@
 import '../styles/globals.css';
 
+// components
+import Head from 'next/head';
 import Layout from '../components/Layout';
+import Transition from '../components/Transition';
+
+// router
+import { useRouter} from 'next/router';
+
+// framer motion
+import { AnimatePresence, motion } from 'framer-motion';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
+    <>
+    <Head>
+        <title>Riya's Portfolio</title>
+        <link rel="icon" href="/bg-exploion.png" />
+    </Head>
     <Layout>
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait">
+        <motion.div key={router.route} className="h-full" style={{height: "99vh"}}>
+          <Transition/>
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </Layout>
+    </>
   );
 }
 
